9b100cee9c21b5db8a86f5ffdd8068048bc9ae71
EMINAVA <emiliano230104@gmail.com>
2022-09-21 21:54:00 +0200

    DocumentPage

--- src/main/java/it/eforhum/corsojava/documents/DocumentPage.java
+++ src/main/java/it/eforhum/corsojava/documents/DocumentPage.java
@@ -3,26 +3,60 @@ package it.eforhum.corsojava.documents;
 import java.util.List;
 
 public class DocumentPage {
-	private List<Document> documents;
-	private int totalResults;
-	private int totalPages;
-	
-	public List<Document> getDocuments() {
-		return documents;
-	}
-	public int getTotalResults() {
-		return totalResults;
-	}
-	public int getTotalPages() {
-		return totalPages;
-	}
-	private void setDocuments(List<Document> documents) {
+    private final List<Document> documents;
+	private final int totalPages;
+	private final int totalResults;
+
+	private DocumentPage(List<Document> documents, int totalPages, int totalResults) {
 		this.documents = documents;
-	}
-	private void setTotalResults(int totalResults) {
+		this.totalPages = totalPages;
 		this.totalResults = totalResults;
 	}
-	private void setTotalPages(int totalPages) {
-		this.totalPages = totalPages;
+
+	public List<Document> getDocuments() {
+        return documents;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+
+    public static class Builder {
+        private List<Document> documents;
+		private int totalPages;
+		private int totalResults;
+
+        public Builder() {
+            this.totalResults = 0;
+            this.totalPages = 0;
+        }
+
+        public DocumentPage buildDocPage() {
+            return new DocumentPage(documents, totalPages, totalResults);
+        }
+
+        public Builder setDocuments(List<Document> documents) {
+            this.documents = documents;
+            return this;
+        }
+
+        public Builder setTotalResults(int totalResults) {
+            this.totalResults = totalResults;
+            return this;
+        }
+
+        public Builder setTotalPages(int totalPages) {
+            this.totalPages = totalPages;
+            return this;
+        }
+    }
+
+	public static DocumentPage createDocumentPage(List<Document> documents, int totalPages, int totalResults) {
+		return new DocumentPage(documents, totalPages, totalResults);
 	}
 }
